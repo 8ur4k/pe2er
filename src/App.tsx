0e980de302023c6e1ff783e9dae7a70ce72d41bf
8ur4k <nobleworms@hotmail.com>
2022-07-05 23:00:55 +0300

    Change baseURL

--- src/App.tsx
+++ src/App.tsx
@@ -53,7 +53,7 @@ function App() {
     const queryParams = new URLSearchParams(document.location.search);
     const callAdress = queryParams.get("to");
     const clientAddress = genRand(7);
-    let baseUrl = "http://127.0.0.1:3000/";
+    let baseUrl = "pe2er/";
     currentLinkInput.value = `${baseUrl}?to=${clientAddress}`;
     var peer = new Peer(clientAddress);
 
